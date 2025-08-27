100c272b79bda83052c71f7f3bd852f11af0a8bf
rudraezz22 <proop500@gmail.com>
2025-08-27 18:53:03 +0530

    Add files via upload

---
+++ bitwise operators.py
@@ -0,0 +1,24 @@
+
+
+
+n = 0
+
+def evencheck(n):
+    if n^1 == n+1:
+        print("it is an even number")
+    else:
+        print("it is an odd number")
+
+evencheck(n)
+#bits check
+
+n = 19
+count=0
+while n :
+    count+=1
+    n>>=1
+print(count)
+
+
+
+print(6^4|2&3)
